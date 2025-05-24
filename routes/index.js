0b4770f5cdb26bb41c66dd2f9b5d48508e487a0e
Omar Cortes <omar.cortes.casillas@gmail.com>
2025-05-24 15:46:10 -0600

    conexion a sql server

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const { poolPromise } = require("../config/db.js");
+
 const router = express.Router();
 
 // Placeholder de futuras rutas
 router.get("/", (req, res) => {
   res.send("📦 CaliforniaClean API en Node.js");
+});
+
+router.get("/dbtest", async (req, res) => {
+  try {
+    const pool = await poolPromise;
+    const result = await pool.request().query("SELECT TOP 5 * FROM Projects");
+    res.json(result.recordset);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const routes = require('./routes');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const routes = require("./routes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.use('/api', routes);
+app.use("/api", routes);
 
+// Inicio puerto
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
