90d018cda1108da623941f4b0fae81fe4d28142d
Omar Cortes <omar.cortes.casillas@gmail.com>
2025-05-24 13:38:12 -0600

    "🚀 Inicializa proyecto CaliforniaClean con Express"

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+// Placeholder de futuras rutas
+router.get("/", (req, res) => {
+  res.send("📦 CaliforniaClean API en Node.js");
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.use('/api', routes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 API CaliforniaClean corriendo en http://localhost:${PORT}`);
+});
